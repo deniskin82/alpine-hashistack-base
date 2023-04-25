e1c94124761b8d0dcdd785f9a156626c38a939d4
deniskin82 <386949+deniskin82@users.noreply.github.com>
2023-04-25 18:07:35 -0400

    Update 00-go-task.sh
    install task from assets

--- scripts/00-go-task.sh
+++ scripts/00-go-task.sh
@@ -1,25 +1,12 @@
 #!/bin/sh
 echo "Install go-task"
+. ~/taskfiles/files/base.cfg
+
 set -xe
 # https://github.com/go-task/task/releases
-TASK_VER="v3.23.0"
-OS_ARCH="$(apk --print-arch)";
-
-apk add curl tar
-
-case "$OS_ARCH" in
-    x86_64) TASK_ARCH="amd64" ;;
-    aarch64) TASK_ARCH="arm64" ;;
-    armv5*) TASK_ARCH="arm" ;;
-    armv6*) TASK_ARCH="arm" ;;
-    armv7*) TASK_ARCH="arm" ;;
-    armhf) TASK_ARCH="arm" ;;
-    *) echo >&2 "error: unsupported architecture ($OS_ARCH)"; exit 1 ;;
-esac;
-
-curl -sSL https://github.com/go-task/task/releases/download/${TASK_VER}/task_linux_${TASK_ARCH}.tar.gz -o /tmp/task_linux.tar.gz
-tar -C /usr/bin/ -zxf /tmp/task_linux.tar.gz task
-rm -fv /tmp/task_linux.tar.gz
+TASK_PKG="task_linux_amd64_${TASK_VERSION}.tar.gz"
+tar -C /usr/bin/ -zxf ~/tools/${TASK_PKG} task
+rm -fv ~/tools/${TASK_PKG}
 task --version
 
 sync
